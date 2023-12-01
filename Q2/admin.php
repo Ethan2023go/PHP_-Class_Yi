18419e7fb2f686ee4985790822a46cbd0cdd448a
Ethan <easonbest123@gmail.com>
2023-12-01 13:13:17 +0800

    增加更多選項 & 表單

--- Q2/admin.php
+++ Q2/admin.php
@@ -1,3 +1,4 @@
+<?php include_once "db.php"?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,12 +9,12 @@
 </head>
 <body>
     <header class="container">
-        <h1 class="text-center">問卷管理</h1>
+        <h1 class="text-center mt-4">問卷管理</h1>
     </header>
     <main class="container p-3">
      <fieldset>
         <legend>新增問卷</legend>
-        <form action="add_que.php" method="post">
+        <form action="api/add_que.php" method="post">
             <!-- 主題 -->
             <div class="d-flex">
                 <div class="col-3 bg-light p-2">問卷選項</div>
@@ -23,10 +24,10 @@
             </div>
             <!-- 選項 -->
                 <div class="bg-light">
-                    <div>
+                    <div class="p-2" id="option">
                         <label for="">選項</label>
                         <input type="text" name="opt[]">
-                        <input type="button" value="更多">
+                        <input type="button" value="更多" onclick="more()">
                     </div>
                 </div>                
             <div class="mt-4">
@@ -35,11 +36,55 @@
             </div>
         </form>
      </fieldset>
+      
 
-    </main>
+       <fieldset>
+        <legend>問卷調查</legend>
+        <div class="col-9 mx-auto">
+        <table class="table">
+            <tr>
+                <td>編號</td>
+                <td>主題內容</td>
+                <td>操作</td>
+            </tr>
+            <?php
+             $ques=$Que->all(['subject_id'=>0]);
+             foreach($ques as $idx =>$que){
+            ?>
+        <tr>
+            <td><?=$idx+1;?></td>
+            <td><?=$que['text'];?></td>
+            <td>
+                <button class="btn btn-info">顯示</button>
+                <button class="btn btn-success">編輯</button>
+                <a href="./api/del.php?id<?=$que['id'];?>">
+                   <button class="btn btn-danger">刪除</button>
+                </a>
+            </td>
+        </tr>
+        <?php
+             }
+        ?>
     
+            </table>
+        </div>
+    </fieldset>   
+  </main>
+             
     
-    <script src="../js/jquery-3.4.1.min.js"></script>
+    <script src="../js/jquery-3.4.1.js"></script>
     <script src="../js/bootstrap.js"></script>
 </body>
 </html>
+
+<script>
+function more(){
+    let opt=`<div class="p-2">
+                <label for="">選項</label>
+                <input type="text" name="opt[]">
+            </div>`
+            $("#option").before(opt)
+}
+
+</script>
+<!-- javascript 原生斷行會認為不同的東西 ``為新版javascript 文字模板 -->
